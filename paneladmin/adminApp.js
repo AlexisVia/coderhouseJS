b7943cdd1bfc872d09ee823e9f35b30c3d48c5ab
alexisviazzo <j.alexisviazzo@gmail.com>
2022-03-18 19:36:13 -0300

    Funcionando

--- paneladmin/adminApp.js
+++ paneladmin/adminApp.js
@@ -2,7 +2,6 @@
 let dia = 1
 let mes = 1
 let year = 1111
-let fecha = dia+"/"+mes+"/"+year
 let direccion = "admin"
 let usuario = "admin"
 let operadorInmobiliario = "admin"
@@ -30,28 +29,15 @@ function noTexto()
 {
     alert("Solo se admiten Numeros. Intenta Nuevamente")
 }
+function mostrarDatos(){
+    alert("En el dia "+dia+"/"+mes+"/"+year+" el " +usuario+" le ha asignado al operador inmobiliario "+operadorInmobiliario+" quien tiene como telefono de contacto "+telContacto+" al propietario "+propietario+" quien posee una propiedad ubicada en "+direccion+" con una superficie total de "+superficieTotal+" mts2 y un area privada de "+areaPrivada+" mts2. Cuenta con "+dormitorios+" dormitorios, "+banos+" baños, y una antiguedad de "+antiguedad+" años. El valor de los gastos comunes es de $ "+gastosComunes+" Para esta propiedad el usuario ha agregado las siguientes notas: "+notas)
+    alert("Hasta pronto "+usuario+" que tengas buen dia")
+}
 
-// Ingreso de datos generales
-function ingresarDatos(){
-    usuario = prompt("Ingresa tu nombre de usuario");
-    while (!isNaN(usuario)) 
-    {
-        noNumeros()
-        usuario = prompt("Ingresa tu nombre de usuario");
-    }
-    alert("Bienvenido " + usuario + " Por favor, completa los siguientes datos de la propiedad");
-    ingresarOperadorInmobiliario()
-    ingresarTelContacto()
-    ingresarPropietario()
-    ingresarSuperficieTotal()
-    ingresarAreaPrivada()
-    ingresarAmbientes()
-    ingresarDormitorios()
-    ingresarbanos()
-    ingresarAntiguedad()
-    ingresarGastosComunes()
-    ingresarNotas()
-} 
+function noMostrarDatos(){
+    alert("Hasta pronto "+usuario+" que tengas buen dia")
+}
+
 // Ingreso de nombre de operador inmobiliario
 function ingresarOperadorInmobiliario(){
     operadorInmobiliario = prompt("Ingresa el nombre del operador inmobiliario asignado");
@@ -63,6 +49,7 @@ function ingresarOperadorInmobiliario(){
 }
 
 // Ingreso de fecha
+
 function ingresarDia(){
     dia = parseInt(prompt("Ingresa el dia"));
     while (isNaN(dia)) 
@@ -70,6 +57,7 @@ function ingresarDia(){
         noTexto()
         dia = prompt("Ingresa el dia");
     }
+    return
 }
 function ingresarMes(){
     mes = parseInt(prompt("Ingresa el mes"));
@@ -78,6 +66,7 @@ function ingresarMes(){
         noTexto()
         mes = prompt("Ingresa el mes");
     }
+    return
 }
 function ingresarYear(){
     year = parseInt(prompt("Ingresa el año"));
@@ -86,6 +75,7 @@ function ingresarYear(){
         noTexto()
         dia = prompt("Ingresa el año");
     }
+    return
 }
 
 // Ingreso de telefono de contacto de operador inmobiliario
@@ -106,6 +96,11 @@ function ingresarPropietario(){
         noNumeros()
         propietario = prompt("Ingresa el nombre del propietario");
     }
+}
+
+// Ingreso de la dirreccion de la propiedad
+function ingresarDireccion(){
+    direccion = prompt("Ingresa la direccion de la propiedad");
 }
 
 // Ingreso de Superficie Total de la Propiedad
@@ -184,9 +179,8 @@ function ingresarGastosComunes(){
 function ingresarNotas(){
     notas = prompt("Ingresa detalles extras que creas pertinentes");
 }
-console.log("hasta aqui llego")
 
-function mostrarDatos(){    
+function opcionMostrar(){    
     
     let respuesta = prompt("Desea comprobar los datos ingresados? SI / NO").toUpperCase();
     
@@ -204,19 +198,51 @@ function mostrarDatos(){
         alert("Respuesta Incorrecta. Seleccione SI o NO")
         respuesta = prompt("Desea comprobar los datos ingresados? SI / NO").toUpperCase;
     }
-    if (respuesta === "SI" || respuesta === "S"  ){
-        alert("En el dia "+fecha+" el usuario "+" le ha asignado al operador inmobiliario "+operadorInmobiliario+" quien tiene como telefono de contacto "+telContacto+" al propietario"+propietario+" quien tiene una propiedad con una propiedad ubicada en "+direccion+" con una superficie total de "+superficieTotal+" mts2 y un area privada de "+" mts2. Cuenta con "+dormitorios+" dormitorios, "+banos+" baños, y una antiguedad de "+antiguedad+" años. El valor de los gastos comunes es de $ "+gastosComunes+" Para esta propiedad el usuario ha agregado las siguientes notas: "+notas)
-        alert("Hasta pronto "+usuario+" que tengas buen dia")
+    if (respuesta == "SI" || respuesta === "S"  ){
+        mostrarDatos()
         }
     else if (respuesta === "NO" || respuesta === "N" ){
-        alert("Hasta pronto "+usuario+" que tengas buen dia")
+        noMostrarDatos()
         } 
     else {
         alert("Respuesta Incorrecta. Seleccione SI o NO")
         respuesta = prompt("Desea comprobar los datos ingresados? SI / NO").toUpperCase();
         }
-    }   
-        
+    
+    }  
+
+
+// Ingreso de datos generales para el inicio del programa
+function ingresarDatos(){
+    usuario = prompt("Ingresa tu nombre de usuario");
+    while (!isNaN(usuario)) 
+    {
+        noNumeros()
+        usuario = prompt("Ingresa tu nombre de usuario");
+    }
+    alert("Bienvenido " + usuario + " Por favor, completa los siguientes datos de la propiedad");
+    ingresarDia()
+    ingresarMes()
+    ingresarYear()
+    ingresarOperadorInmobiliario()
+    ingresarTelContacto()
+    ingresarPropietario()
+    ingresarDireccion()
+    ingresarSuperficieTotal()
+    ingresarAreaPrivada()
+    ingresarAmbientes()
+    ingresarDormitorios()
+    ingresarbanos()
+    ingresarAntiguedad()
+    ingresarGastosComunes()
+    ingresarNotas()
+    opcionMostrar()
+} 
+
+
+ 
+    
+   
     
     
 
@@ -226,5 +252,4 @@ ingresarDatos()
 
 //La idea a futuro sera que la funcion de MOSTRAR datos se active con un clik en un boton
 
-mostrarDatos()
 
