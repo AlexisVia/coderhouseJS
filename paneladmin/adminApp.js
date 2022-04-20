4127b618982258a00eb8909922b2a87d258a7e74
alexisviazzo <j.alexisviazzo@gmail.com>
2022-04-20 17:59:03 -0300

    Operador Ternario. Operador Logico &&, Operador OR ||, destructuracion, alias, spread

--- paneladmin/adminApp.js
+++ paneladmin/adminApp.js
@@ -59,15 +59,16 @@ function nuevaPropiedad(){
         confirmButtonText: 'Aceptar'
 })
 document.getElementById("formulario").reset();
+comprobacion()
     
 }
-//****************************Llamado a funcion para AGREGAR PROPIEDAD**********************************************
+//****************************Funcion para AGREGAR PROPIEDAD**********************************************
 //Dentro de esta funcion ya corre la opcion de MOSTRAR por alerta los ultimos datos ingresados
 function agregar(){
     const guardarPropiedad = document.getElementById("guardarPropiedad");
     guardarPropiedad.onclick = () =>{nuevaPropiedad()}
 }
-//****************************Llamado a funcion para BUSCAR PROPIEDAD**********************************************
+//****************************Funcion para BUSCAR PROPIEDAD**********************************************
 
 const buscarPropiedad = document.getElementById("buscando");
 buscarPropiedad.addEventListener("click", (e) =>{
@@ -77,14 +78,26 @@ buscarPropiedad.addEventListener("click", (e) =>{
 
 function buscar(){
     const qBuscar = document.getElementById("qBuscar").value;
-    let propiedad = JSON.parse(localStorage.getItem("PROPIEDAD_AGREGADA"));
-    var busquedaOp = propiedad.opInmobiliario;
-    var busquedaNombre = propiedad.nombrePropietario;
-    var busquedaTelefono = propiedad.direccionPropiedad;
-    var busquedaDireccion = propiedad.direccionPropiedad;
-
-    
-    if (qBuscar != busquedaOp){
+    let propiedad = JSON.parse(localStorage.getItem("PROPIEDAD_AGREGADA")) ;
+if (propiedad == undefined){
+    Swal.fire({
+        title: 'Vacio',
+        text: 'No se ha agregado ninguna propiedad',
+        icon: 'error',
+        confirmButtonText: 'Cerrar'
+    })
+}
+else{
+    const { opInmobiliario:opInmobiliarioEncontrado,
+         nombrePropietario:nombrePropietarioEncontrado,
+         direccionPropiedad:direccionPropiedadEncontrada,
+         telefonoPropietario:telefonoPropietarioEncontrado } = propiedad //desestructurizado y alias
+    // var busquedaOp = propiedad.opInmobiliario;
+    // var busquedaNombre = propiedad.nombrePropietario;
+    // var busquedaTelefono = propiedad.direccionPropiedad;
+    // var busquedaDireccion = propiedad.telefonoPropietario;
+ 
+    if (qBuscar != opInmobiliarioEncontrado){
         Swal.fire({
             title: 'No encontrado',
             text: 'La Propiedad Buscada no existe',
@@ -93,22 +106,37 @@ function buscar(){
         })
     }
     else {
-        if (busquedaOp != null){
-            Swal.fire({
-                title: 'Propiedad Encontrada',
-                icon: 'success',
-                confirmButtonText: 'Ver'
-        })
-        renderPropiedadEncontrada()
-        }
-        else{
-            Swal.fire({
-                title: 'No encontrado',
-                text: 'La Propiedad Buscada no existe',
-                icon: 'error',
-                confirmButtonText: 'Cerrar'
-            })
-        }
+        opInmobiliarioEncontrado != null ? Swal.fire({
+                    title: 'Propiedad Encontrada',
+                    icon: 'success',
+                    confirmButtonText: 'Ver'
+                    }) &&
+                    renderPropiedadEncontrada() 
+                    :
+                    Swal.fire({
+                    title: 'No encontrado',
+                    text: 'La Propiedad Buscada no existe',
+                    icon: 'error',
+                    confirmButtonText: 'Cerrar'
+                    })
+
+        // IDEM ARRIBA SIN OPERADOR TERNARIO NI LOGICO
+        // if (busquedaOp != null){
+        //     Swal.fire({
+        //         title: 'Propiedad Encontrada',
+        //         icon: 'success',
+        //         confirmButtonText: 'Ver'
+        // })
+        // renderPropiedadEncontrada()
+        // }
+        // else{
+        //     Swal.fire({
+        //         title: 'No encontrado',
+        //         text: 'La Propiedad Buscada no existe',
+        //         icon: 'error',
+        //         confirmButtonText: 'Cerrar'
+        //     })
+        // }
 }
 function renderPropiedadEncontrada(){
     let propiedadEncontrada = document.getElementById("render")
@@ -116,13 +144,13 @@ function renderPropiedadEncontrada(){
         <div class="card content-card">
             <div class="card-body">
             <h3>Las siguientes propiedades coinciden con su busqueda</h3>
-              <h5 class="card-title">Propiedad en ${busquedaDireccion}</h5>
+              <h5 class="card-title">Propiedad en ${direccionPropiedadEncontrada}</h5>
             </div>
             <ul class="list-group list-group-flush">
-              <li class="list-group-item"><i class="fa-thin fa-person"></i> Operador Inmobiliario asignado ${busquedaOp}</li>
+              <li class="list-group-item"><i class="fa-thin fa-person"></i> Operador Inmobiliario asignado ${opInmobiliarioEncontrado}</li>
               <li class="list-group-item"><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span><span class="fa-solid fa-star"></span></li>
-              <li class="list-group-item"><i class="fa-thin fa-mobile-screen-button"></i></i> Telefono de Contacto: ${busquedaTelefono}</li>
-              <li class="list-group-item"><i class="fa-solid fa-house-chimney"></i> Propietario: ${busquedaNombre}</li>
+              <li class="list-group-item"><i class="fa-thin fa-mobile-screen-button"></i></i> Telefono de Contacto: ${telefonoPropietarioEncontrado}</li>
+              <li class="list-group-item"><i class="fa-solid fa-house-chimney"></i> Propietario: ${nombrePropietarioEncontrado}</li>
             </ul>
             <div class="card-body">
                 <button type="button" class="btn btn-success">Publicar</button>
@@ -132,19 +160,51 @@ function renderPropiedadEncontrada(){
     </div>`
     }
 }
+}
+
+//****************************Funcion para ELIMINAR PROPIEDAD**********************************************
 
 
 
 
 
+//****************************Funcion para COMPROBAR ULTIMA PROPIEDAD AGREGADA**********************************************
 
+function comprobacion(){
+    const ultimaPropiedad = JSON.parse( localStorage.getItem('PROPIEDAD_AGREGADA') ) || []
+    var comprobacionOp = ultimaPropiedad.opInmobiliario;
+    // var busquedaNombre = propiedad.nombrePropietario;
+    // var busquedaTelefono = propiedad.direccionPropiedad;
+    // var busquedaDireccion = propiedad.direccionPropiedad;
+    let renderUltimaPropiedad = document.getElementById("ultimaPropiedadAgregada");
+    renderUltimaPropiedad.innerHTML = ` <h5>La ultima propiedad ha sido agregada por <span class="badge bg-info">${comprobacionOp}</span></h5> `
+}
 
-//****************************Llamado a funcion para ELIMINAR PROPIEDAD**********************************************
+//****************************Funcion para LISTAR OPERADORES INMOBILIARIOS AGREGADOS**********************************************
+function listarOp(){
+    const proximamente = document.getElementById("listarOp");
+    proximamente.addEventListener("click", (e) =>{
+    const comprobacionListar = JSON.parse( localStorage.getItem('PROPIEDAD_AGREGADA') )
+    // console.log(comprobacionListar)
+    var listar = comprobacionListar.opInmobiliario;
+    // console.log(listar)
+    const nombresObj = {
+    ...listar
+}
+console.log(nombresObj)
+console.log("Proximamente se podra listar en DOM")
+})
+}
+
 
 
 //****************************Llamado a funciones cargadas en el BODY**********************************************
 function loadBody(){
     agregar()
+    comprobacion()
+    listarOp()  
 }
 loadBody()
+
+
 
